5ce11e10a345814f6dbb14eb63c9f338465391a5
Ainur Minibaev <rapper01@list.ru>
2014-06-18 02:39:27 +0400

    services module main

---
+++ services/src/com/app/citizen/services/activity/ServicesMainActivity.java
@@ -0,0 +1,36 @@
+package com.app.citizen.services.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import com.app.citizen.services.R;
+
+/**
+ * Created by ainurminibaev on 18.06.14.
+ */
+public class ServicesMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_services);
+        findViewById(R.id.btn_fines).setOnClickListener(clickListener);
+        findViewById(R.id.btn_hospital).setOnClickListener(clickListener);
+    }
+
+    private final View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = null;
+            int i = v.getId();
+            if (i == R.id.btn_fines) {
+                intent = new Intent(getApplicationContext(), FineActivity.class);
+
+            } else if (i == R.id.btn_hospital) {
+                intent = new Intent(getApplicationContext(), HospitalActivity.class);
+            }
+            startActivity(intent);
+        }
+    };
+}
